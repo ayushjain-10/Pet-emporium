8dc62cd967ff440bbaed093a6012c8fb07e4c483
Ayush Jain <ayushshreeshreemal@gmail.com>
2022-06-01 06:05:10 -0700

    updates

--- seeds/pets.js
+++ seeds/pets.js
@@ -4,9 +4,9 @@ module.exports = [
         "species": "Rottweiler",
         "birthday": "20017-11-11",
         "favoriteFood": "Chicken",
-        "picUrl": "http://www.dogbreedslist.info/uploads/allimg/dog-pictures/Rottweiler-3.jpg",
+        "picUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Rottweiler_standing_facing_left.jpg/800px-Rottweiler_standing_facing_left.jpg",
         "price": 9.99,
-        "picUrlSq": "http://www.dogbreedplus.com/dog_breeds/images/cute-rottweiler-puppy.jpg",
+        "picUrlSq": "https://www.purina.co.uk/sites/default/files/2021-02/BREED%20Hero_0109_rottweiler.jpg",
         "description": "Rex is a dog and he's a good dog who loves to play and hang out with his owners. He also likes to nap and enjoys eating dog food"
     },
     {
@@ -34,7 +34,7 @@ module.exports = [
         "species": "West Highland White Terrier",
         "birthday": "2008-11-11",
         "favoriteFood": "Fish",
-        "picUrl": "http://www.dogbreedslist.info/uploads/allimg/dog-pictures/West-Highland-White-Terrier-2.jpg",
+        "picUrl": "https://patterjack.com/wp-content/uploads/2021/11/West-Highland-White-Terrier_anetapics_Shutterstock-1.jpg",
         "picUrlSq": "https://upload.wikimedia.org/wikipedia/commons/2/2c/West_Highland_White_Terrier_Krakow.jpg",
         "description": "Princhi is a dog and he's a good dog who loves to play and hang out with his owners. He also likes to nap and enjoys eating dog food"
     },
@@ -43,7 +43,7 @@ module.exports = [
         "species": "Poodle",
         "birthday": "2008-11-11",
         "favoriteFood": "Chicken",
-        "picUrl": "https://i.ytimg.com/vi/-57vwuw_aY8/maxresdefault.jpg",
+        "picUrl": "https://vetstreet.brightspotcdn.com/dims4/default/b5fcb62/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F38%2Fec%2Ffd4eecbe4f57a282b707f097ef33%2Fpoodle-ap-1j99ae-645-x-380.jpg",
         "price": 9.99,
         "picUrlSq": "http://www.caninest.com/images/black-poodle.jpg",
         "description": "Mr. Fluffles is a dog and he's a good dog who loves to play and hang out with his owners. He also likes to nap and enjoys eating dog food"
@@ -53,9 +53,9 @@ module.exports = [
         "species": "Mixed",
         "birthday": "2008-11-11",
         "favoriteFood": "Pork",
-        "picUrl": "http://via.placeholder.com/350x150",
+        "picUrl": "https://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/8/2020/03/GettyImages-512366437-e1583519258231.jpg",
         "price": 9.99,
-        "picUrlSq": "http://via.placeholder.com/250x250",
+        "picUrlSq": "https://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/8/2020/03/GettyImages-512366437-e1583519258231.jpg",
         "description": "Santa's Little Helper is a dog and he's a good dog who loves to play and hang out with his owners. He also likes to nap and enjoys eating dog food"
     },
     {
@@ -63,9 +63,9 @@ module.exports = [
         "species": "Alien",
         "birthday": "2008-11-11",
         "favoriteFood": "Cats",
-        "picUrl": "http://via.placeholder.com/350x150",
+        "picUrl": "https://i.guim.co.uk/img/media/364089a4778d97eac2be1a48913931b6f562e169/920_1562_2138_1282/master/2138.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=c6b604fb567ffa5e9f8172a4c82a7ec5",
         "price": 59.99,
-        "picUrlSq": "http://via.placeholder.com/250x250",
+        "picUrlSq": "https://i.guim.co.uk/img/media/364089a4778d97eac2be1a48913931b6f562e169/920_1562_2138_1282/master/2138.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=c6b604fb567ffa5e9f8172a4c82a7ec5",
         "description": "Alf is an alien and he's a good alien who loves to play and hang out with his owners. He also likes to nap and enjoys eating cats"
     },
     {
@@ -73,9 +73,9 @@ module.exports = [
         "species": "Mastiff",
         "birthday": "2008-11-11",
         "favoriteFood": "Dragon",
-        "picUrl": "http://via.placeholder.com/350x150",
+        "picUrl": "https://api.time.com/wp-content/uploads/2017/04/world-of-dogs-07.jpg?quality=85&w=1200&h=628&crop=1",
         "price": 9.99,
-        "picUrlSq": "http://via.placeholder.com/250x250",
+        "picUrlSq": "https://api.time.com/wp-content/uploads/2017/04/world-of-dogs-07.jpg?quality=85&w=1200&h=628&crop=1",
         "description": "Bubba is a dog and he's a good dog who loves to play and hang out with his owners. He also likes to nap and enjoys eating dog food"
     }
     
